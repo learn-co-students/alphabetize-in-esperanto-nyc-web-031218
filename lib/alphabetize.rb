f9f317b1ad9d5b6718a371c71ee75c1eb2bbb77a
J.D. Bean <jonathandbean@gmail.com>
2018-02-25 19:00:12 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,17 @@
+# require 'pry'
+
+# #alphabetize
+#   alphabetizes based on the first letter
+#   alphabetizes based on second letters
+#   alphabetizes based on sixth letters
+#   alphabetizes based on second letters not in English alphabet
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |phrase|
+    phrase.split("").map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
 end
