d30125232d6b4d2e11a6d1d0aebdb9adf06ae393
Harim Choi <harim1206@gmail.com>
2018-03-04 18:11:15 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,40 +1,26 @@
 require "pry"
 
+def esperanto
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+end
 
-
-arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
-
-
-input = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
-[m,i, ,a,m,a,s, ,v,i,n]
-m -> "16"
-[b,o,n,a,n]
-[3,4,3,6]
-[16,5,4,6]
 
 
 def alphabetize(arr)
   # code here
-  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  #arr.sort_by{ |item| }
 
-  arr.each{ |string|
+  x = arr.sort_by{ |string|
 
-    char_array = string.split("")
-
-    char_array.map{ |char|
-      binding.pry
+    string.split("").map{ |char|
 
       char = esperanto.index(char)
 
     }
-
-
   }
-
-
-
-
-  turn each string to an array of integers of esperanto indices
+  puts x
+  return x
+  #binding.pry
 
 end
 
