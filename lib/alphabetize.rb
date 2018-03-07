c73835b03dce1a73b0fa05d05df71703626f4881
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-03-07 05:45:13 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,20 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  i = 0
+  array = []
+  arr.each do |word|
+    array << ESPERANTO_ALPHABET.index(word[i])
+  end
+  until array.uniq.length == array.length
+    array = []
+    i += 1
+    arr.each do |word|
+      array << ESPERANTO_ALPHABET.index(word[i])
+    end
+  end
+  arr.sort_by do |word|
+    ESPERANTO_ALPHABET.index(word[i])
+  end
 end
