4af6787902755a4d36cb7c91fd008b8efd1b9f4a
Roger Perez <rogercodes1@gmail.com>
2018-03-10 23:07:11 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,21 @@
-def alphabetize(arr)
-  # code here
+require "pry"
+
+# ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(array)
+  esperanto=ESPERANTO_ALPHABET
+  sorted_alpha={}
+  # binding.pry
+  esperanto.split('').each_with_index.map do |alpha, index|
+    sorted_alpha[index]=alpha
+  end
+  # binding.pry
+  final=[]
+array.sort_by do |element|
+    final=array
+    element.split('').map do |letter|
+      sorted_alpha.index(letter)
+    end
+  end
 end
